b668378896fb4a46b73e04e337c3084878c79b95
Mahfuz Zayn <mahfuzzayn8@gmail.com>
2025-08-21 20:02:48 +0600

    Added Description to FirstClass File

--- Starter (Basic)/firstPackage/FirstClass.java
+++ Starter (Basic)/firstPackage/FirstClass.java
@@ -1,3 +1,14 @@
+/*
+
+Programmed by: Mahfuz Zayn
+Date created: 10 August, 2025
+Last updated: 21 August, 2025
+
+Description
+First Java application which prints "Hello World" in terminal.
+ 
+*/
+
 package firstPackage;
 
 public class FirstClass {
