31e2789e5d3fb1043e3fdfe36de807bc02b901d6
Mahfuz Zayn <mahfuzzayn8@gmail.com>
2025-08-25 01:18:25 +0600

    Update of BankAccountSystem Part-1

--- Starter (Basic)/programs/BankAccountSystem.java
+++ Starter (Basic)/programs/BankAccountSystem.java
@@ -1,7 +1,61 @@
 package programs;
+import java.util.Scanner;
 
 public class BankAccountSystem {
-	public static void main(String[] args) {
+	static double deposit(double balance, double amount) {
+		return balance + amount;
+	}
+	
+	static double withdraw(double balance, double amount) {
+		if (amount > balance) return balance;
 		
+		return balance - amount;
+	}
+	
+	static double checkBalance(double balance) {
+		return balance;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanf = new Scanner(System.in);
+		
+		String name;
+		int accountNumber;
+		double balance;
+		
+		System.out.println("1. Create Account\n2. Deposit\n3. Withdraw\n4. Check Balance\n5. Exit");
+		
+		name = scanf.nextLine();
+		accountNumber = 101;
+		balance = scanf.nextDouble();
+		
+		while (true) {
+			System.out.println("1. Create Account\n2. Deposit\n3. Withdraw\n4. Check Balance\n5. Exit");
+			
+			int option = scanf.nextInt();
+			
+			switch (option) {
+				case 2: 
+					double deposit_amount = scanf.nextDouble();
+					
+					deposit(balance, deposit_amount);
+					
+					break;
+				case 3: 
+					double withdrawal_amount = scanf.nextDouble();
+					
+					withdraw(balance, withdrawal_amount);
+					
+					break;
+				case 4:
+					checkBalance(balance);
+					
+					break;
+				case 5:
+					break;
+			}
+		}
+		
+		scanf.close();
 	}
 }
