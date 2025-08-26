317541ce6a790f1525d860d777a1d65e01945c41
Mahfuz Zayn <mahfuzzayn8@gmail.com>
2025-08-27 02:27:12 +0600

    Update of BankAccountSystem Part-4: Fully Functional Version 1.00

--- Starter (Basic)/programs/BankAccountSystem.java
+++ Starter (Basic)/programs/BankAccountSystem.java
@@ -21,9 +21,8 @@ public class BankAccountSystem {
 		
 		String name;
 		int accountNumber;
-		double balance;
 		
-		System.out.println("1. Create Account\n2. Deposit\n3. Withdraw\n4. Check Balance\n5. Exit");
+		System.out.println("1. Create Account\n2. Deposit\n3. Withdraw\n4. Check Balance\n5. Exit\n");
 		
 		int option = scanf.nextInt();
 		
@@ -33,43 +32,50 @@ public class BankAccountSystem {
 		}
 		
 		System.out.println("Enter your name: ");
-		name = "Mahfuz Zayn";
+		name = scanf.next();
 		
-		accountNumber = 101;
+		accountNumber = 20250827;
 		
 		System.out.println("Initial balance: ");
-		balance = 500;
+		double balance = scanf.nextDouble();
 		
-		System.out.println("\n");
-		System.out.printf("Welcome, %s\n", name);
-		System.out.printf("Account balance: %f\n\n", balance);
+		System.out.printf("\nWelcome, %s\n", name);
+		System.out.printf("Account number %d and balance: %.2f taka\n\n", accountNumber, balance);
 		
 		while (true) {
-			System.out.println("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit");
+			System.out.println("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit\n");
 			
 			option = scanf.nextInt();
 			
 			switch (option) {
-				case 2: 	
+				case 1: 	
+					System.out.printf("Enter your deposit amount: ");
 					double deposit_amount = scanf.nextDouble();
 					
-					deposit(balance, deposit_amount);
+					balance = deposit(balance, deposit_amount);
+					System.out.printf("Deposit sucessful. Current balance: %.2f taka \n\n", balance);
 					
 					break;
-				case 3: 
+				case 2: 
+					System.out.printf("Enter your withdrawal amount: ");
 					double withdrawal_amount = scanf.nextDouble();
 					
-					withdraw(balance, withdrawal_amount);
+					balance = withdraw(balance, withdrawal_amount);
+					System.out.printf("Withdrawal sucessful. Current balance: %.2f taka \n\n", balance);
 					
 					break;
-				case 4:
-					checkBalance(balance);
+				case 3:
+					double current_balance = checkBalance(balance);
+					System.out.printf("Your current balance: %.2f taka \n", current_balance);
+					
 					break;
-				case 5: 
+				case 4: 
 					break;
 			}
 			
-			if (option == 5) {
+			if (option == 4) {
+				System.out.printf("Exiting Program... Thank you.\n");
+				
 				break;
 			}
 		}
