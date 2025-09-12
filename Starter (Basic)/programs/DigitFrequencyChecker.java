c4f16ff75dd46834f2aa5ca198e9d5fd876ffdc9
Mahfuz Zayn <mahfuzzayn8@gmail.com>
2025-09-12 23:46:02 +0600

    Updated DigitFrequencyChecker: Added Scanner and Input taking of Digit

--- Starter (Basic)/programs/DigitFrequencyChecker.java
+++ Starter (Basic)/programs/DigitFrequencyChecker.java
@@ -1,7 +1,12 @@
 package programs;
+import java.util.Scanner;
 
 public class DigitFrequencyChecker {
 	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		int digit = input.nextInt();
+
+		// Next step
 	}
 }
