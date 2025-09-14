52e49ffc778910d869b1e2f4c715c5b3c9d10864
Mahfuz Zayn <mahfuzzayn8@gmail.com>
2025-09-14 23:21:19 +0600

    Updated: DigitFrequencyChecker - Used While Loop, %, / Checkings etc.

--- Starter (Basic)/programs/DigitFrequencyChecker.java
+++ Starter (Basic)/programs/DigitFrequencyChecker.java
@@ -6,7 +6,19 @@ public class DigitFrequencyChecker {
 		Scanner input = new Scanner(System.in);
 		
 		int digit = input.nextInt();
+		int[] frequency = new int[10];
 
-		// Next step
+		while (digit != 0) {
+			int d = digit % 10;
+			frequency[d]++;
+			digit /= 10;
+		}
+
+		System.out.println("\nDigit frequencies:");
+		for (int i = 0; i < frequency.length; i++) {
+			System.out.println("Digit " + i + " → " + frequency[i] + " times");
+		}
+		
+		input.close();
 	}
 }
